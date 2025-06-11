367cbb7542ba0cc95fbe711a7b67d0934152447b
Kristers-U <kristeramu@gmail.com>
2025-06-11 12:40:01 +0300

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,34 @@
+function initializeTheme() {
+    let theme = localStorage.getItem("theme");
+    
+    if (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        theme = "dark";
+    } else {
+        theme = theme || "light";
+    }
+
+    document.body.classList.toggle("dark-mode", theme === "dark");
+    return theme;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentTheme = initializeTheme();
+    const themeButtons = document.querySelectorAll(".dark-mode-toggle");
+    
+    //button text
+    themeButtons.forEach(button => {
+        button.textContent = currentTheme === "dark" ? 
+            "Switch to Light Mode" : 
+            "Switch to Dark Mode";
+    });
+
+    themeButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            const newTheme = currentTheme === "dark" ? "light" : "dark";
+            
+            //update theme
+            document.body.classList.toggle("dark-mode");
+            localStorage.setItem("theme", newTheme);
+        });
+    });
+});
